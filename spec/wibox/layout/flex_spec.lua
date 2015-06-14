87bb7853da3f18caee94e9fcee8192192cbed83e
Uli Schlachter <psychon@znc.in>
2015-06-14 16:17:05 +0200

    Convert the flex layout to the new widget system
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- spec/wibox/layout/flex_spec.lua
+++ spec/wibox/layout/flex_spec.lua
@@ -5,6 +5,7 @@
 
 local flex = require("wibox.layout.flex")
 local utils = require("wibox.test_utils")
+local p = require("wibox.widget.base").place_widget_at
 
 describe("wibox.layout.flex", function()
     local layout
@@ -12,16 +13,12 @@ describe("wibox.layout.flex", function()
         layout = flex.vertical()
     end)
 
-    before_each(utils.stub_draw_widget)
-    after_each(utils.revert_draw_widget)
-
     it("empty layout fit", function()
         assert.widget_fit(layout, { 10, 10 }, { 0, 0 })
-        utils.check_widgets_drawn({})
     end)
 
-    it("empty layout draw", function()
-        layout:draw(nil, nil, 0, 0)
+    it("empty layout layout", function()
+        assert.widget_layout(layout, { 0, 0 }, {})
     end)
 
     describe("with widgets", function()
@@ -42,44 +39,39 @@ describe("wibox.layout.flex", function()
                 assert.widget_fit(layout, { 100, 100 }, { 15, 35 })
             end)
 
-            it("draw", function()
-                layout:draw("wibox", "cr", 100, 100)
-                utils.check_widgets_drawn({
-                    { first,  0,  0, 100, 33 },
-                    { second, 0, 33, 100, 33 },
-                    { third,  0, 67, 100, 33 },
+            it("layout", function()
+                assert.widget_layout(layout, { 100, 100 }, {
+                    p(first,  0,  0, 100, 33),
+                    p(second, 0, 33, 100, 33),
+                    p(third,  0, 67, 100, 33),
                 })
             end)
         end)
 
         describe("without enough height", function()
             it("fit", function()
-                -- XXX: Is this really what should happen?
-                assert.widget_fit(layout, { 5, 100 }, { 15, 35 })
+                assert.widget_fit(layout, { 5, 100 }, { 5, 35 })
             end)
 
-            it("draw", function()
-                layout:draw("wibox", "cr", 5, 100)
-                utils.check_widgets_drawn({
-                    { first,  0,  0, 5, 33 },
-                    { second, 0, 33, 5, 33 },
-                    { third,  0, 67, 5, 33 },
+            it("layout", function()
+                assert.widget_layout(layout, { 5, 100 }, {
+                    p(first,  0,  0, 5, 33),
+                    p(second, 0, 33, 5, 33),
+                    p(third,  0, 67, 5, 33),
                 })
             end)
         end)
 
         describe("without enough width", function()
             it("fit", function()
-                -- XXX: Is this really what should happen?
-                assert.widget_fit(layout, { 100, 20 }, { 15, 35 })
+                assert.widget_fit(layout, { 100, 20 }, { 15, 20 })
             end)
 
-            it("draw", function()
-                layout:draw("wibox", "cr", 100, 20)
-                utils.check_widgets_drawn({
-                    { first,  0,  0, 100, 6 },
-                    { second, 0,  7, 100, 6 },
-                    { third,  0, 13, 100, 6 },
+            it("layout", function()
+                assert.widget_layout(layout, { 100, 20 }, {
+                    p(first,  0,  0, 100, 6),
+                    p(second, 0,  7, 100, 6),
+                    p(third,  0, 13, 100, 6),
                 })
             end)
         end)
