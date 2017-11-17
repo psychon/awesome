a8b2ab0d762776662cdf4e03e1452971a60098e0
Uli Schlachter <psychon@znc.in>
2017-11-17 10:50:38 +0100

    Reenable test-menubar.lua
    NOT-signed-off

--- tests/test-menubar.lua
+++ tests/test-menubar.lua
@@ -10,19 +10,6 @@ function menubar.refresh(...)
     orig_refresh(...)
 end
 
--- XXX We are building Lua 5.3 with -DLUA_USE_APICHECK=1 and this catches some
--- bugs in lgi. Thus, do not run this test with Lua 5.3 until lgi is fixed.
--- We run into the same bug when doing code-coverage analysis, supposedly
--- because the additional GC activity means that something which is GC-able when
--- it should not gets collected too early.
--- This test also sporadically errors on LuaJIT ("attempt to call a number
--- value"). This might be a different issue, but still, disable the test.
-if _VERSION == "Lua 5.3" or debug.gethook() or jit then --luacheck: globals jit
-    print("Skipping this test since it would just fail.")
-    runner.run_steps { function() return true end }
-    return
-end
-
 -- Make sure no error messages for non-existing directories are printed. If the
 -- word "error" appears in our output, the test is considered to have failed.
 do
